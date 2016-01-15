28c8e4ef344912f0c2c350bfb6a2416378a2a545
Tezza <Kaweechelchen@users.noreply.github.com>
2016-01-15 23:36:39 +0100

    fixed box design

--- src/ssherver/server.go
+++ src/ssherver/server.go
@@ -19,19 +19,19 @@ import (
 var termTmpl = template.Must(template.New("termTmpl").Parse(strings.Replace(`
     +---------------------------------------------------------------------+
     |                                                                     |
-    |             _o/ Hello {{ .Name }}!
+    |             _o/ Hello {{ .Name }}!                                  |
     |                                                                     |
     |                                                                     |
     |  Did you know that ssh sends all your public keys to any server     |
     |  it tries to authenticate to?                                       |
     |                                                                     |
-    |  That's how we know you are @{{ .User }} on GitHub!
+    |  That's how we know you are @{{ .User }} on GitHub!                 |
     |                                                                     |
     |  Ah, maybe what you didn't know is that GitHub publishes all users' |
     |  ssh public keys and Ben (benjojo.co.uk) grabbed them all.          |
     |                                                                     |
     |  That's pretty handy at times :) for example your key is at         |
-    |  https://github.com/{{ .User }}.keys
+    |  https://github.com/{{ .User }}.keys                                |
     |                                                                     |
     |                                                                     |
     |  P.S. This whole thingy is Open Source! (And written in Go!)        |
